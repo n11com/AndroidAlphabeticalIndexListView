dcad90cb21c6656bdf7e54e9fb95af749f4d8391
Onur Taskin <onur.taskin@n11.com>
2015-03-31 11:19:17 +0300

    added screenshot and updated dummy data

--- app/src/main/java/com/n11/tech/alphabeticalindexlistview/util/Utils.java
+++ app/src/main/java/com/n11/tech/alphabeticalindexlistview/util/Utils.java
@@ -22,44 +22,47 @@ public class Utils {
     public static Map<String, List<String>> testData() {
         Map<String, List<String>> data = new HashMap();
         List<String> a = new ArrayList<String>();
-        a.add("Armani");
-        a.add("Adidas");
-        a.add("Abibas");
+        a.add("Acura");
+        a.add("Audi");
+        a.add("Alfa Romeo");
         List<String> b = new ArrayList<String>();
-        b.add("Bandai");
-        b.add("Bonibon");
-        b.add("Bacardi");
+        b.add("Buick");
+        b.add("BMW");
+        b.add("Bentley");
 
         List<String> c = new ArrayList<String>();
-        c.add("C&A");
-        c.add("Colins");
-        c.add("Ceyo");
-        c.add("Cenk");
+        c.add("Cadillac");
+        c.add("Carver");
+        c.add("Chevrolet");
+        c.add("Chrysler");
+        c.add("Corvette");
 
         List<String> d = new ArrayList<String>();
-        d.add("Dockers");
-        d.add("Dogan");
         d.add("Dodge");
-        d.add("Dolge");
+        d.add("Donkervoort");
+        d.add("Daewoo");
+        d.add("Dacia");
 
-        List<String> e = new ArrayList<String>();
-        e.add("Etsy");
-        e.add("Elle");
-        e.add("Emporio Armani");
-        e.add("Else Silver");
+        List<String> f = new ArrayList<String>();
+        f.add("Ferrari");
+        f.add("Fiat");
+        f.add("Ford");
+        f.add("Fisker");
 
-        List<String> k = new ArrayList<String>();
-        k.add("Koton");
-        k.add("Keds");
-        k.add("Kenzo");
-        k.add("Kawasaki");
+        List<String> l = new ArrayList<String>();
+        l.add("Lamborghini");
+        l.add("Lancia");
+        l.add("Land Rover");
+        l.add("Lexus");
+        l.add("Lincoln");
+        l.add("Lotus");
 
         data.put("A", a);
         data.put("B", b);
         data.put("C", c);
         data.put("D", d);
-        data.put("E", e);
-        data.put("K", k);
+        data.put("F", f);
+        data.put("L", l);
 
         return data;
     }
